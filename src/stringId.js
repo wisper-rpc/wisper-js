29aa5c60a3eab9a0d782ac0802c9907132a9fd5a
Oskar Segersvärd <oskar.segersvard@widespace.com>
2015-07-23 10:58:39 +0200

    Removed unused parameter from `stringId`.

--- src/stringId.js
+++ src/stringId.js
@@ -1,6 +1,7 @@
 // Create string id generating function.
-export default function (id=0) {
+export default function stringId() {
   const base = Math.random().toString(36).slice(2);
+  let id = 0;
 
   return () => base + id++;
 }
