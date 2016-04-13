54268929cfb074c56fd2c0b4f4af6d9162bdcf69
Oskar Segersvärd <oskar.segersvard@widespace.com>
2016-04-13 11:43:37 +0200

    Remove unused `commonjs` plugin

--- rollup.es6.js
+++ rollup.es6.js
@@ -1,6 +1,5 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: 'wisper.js',
@@ -8,6 +7,6 @@ export default {
 
   plugins: [
     babel(),
-    resolve()
-  ]
+    resolve(),
+  ],
 };
