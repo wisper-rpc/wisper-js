5f3e650621c35f3406eaf85d36a363566ed05164
Oskar Segersvärd <oskar.segersvard@widespace.com>
2016-05-18 14:25:20 +0200

    Restructure createClassRouter and interfaceName

--- src/objects/interfaceName.js
+++ src/objects/interfaceName.js
@@ -2,19 +2,21 @@ import EventHandler from '../events.js';
 import ClassRouter from './ClassRouter.js';
 
 
-export const createClassRouter = ClassRouter.routing;
+export function createClassRouter(bridge, name, cls) {
+  // Constructor be called several times on same object.
+  EventHandler.call(cls);
+
+  return ClassRouter.routing(bridge, name, cls);
+};
 
 // Decorator for `Remote` and `Local` classes.
 // Exposes the classes through the bridge.
 export default function interfaceName(bridge, name) {
   return cls => {
-    const router = ClassRouter.routing(bridge, name, cls);
+    const router = createClassRouter(bridge, name, cls);
 
     if (!bridge.expose(name, router)) {
       throw new Error(`Route '${name}' already exposed.`);
     }
-
-    // Constructor be called several times on same object.
-    EventHandler.call(cls);
   };
 }
